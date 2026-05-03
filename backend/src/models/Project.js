282e2802b1d56d01f91e346e230ac5e38abc17c6
Vaibhav <vaibhavshivhare1709@gmail.com>
2026-05-04 01:32:40 +0530

    commit

--- backend/src/models/Project.js
+++ backend/src/models/Project.js
@@ -1,13 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const projectSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, minlength: 3, maxlength: 100 },
     description: { type: String, maxlength: 500 },
-    admin: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    admin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
-module.exports = mongoose.model('Project', projectSchema);
+module.exports = mongoose.model("Project", projectSchema);
